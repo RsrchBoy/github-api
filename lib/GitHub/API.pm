0f0b607d2eea88b0bb666338dd8226894ef581f0
Chris Weyl <cweyl@alumni.drew.edu>
2013-03-27 09:50:24 -0700

    initial commit

---
+++ lib/GitHub/API.pm
@@ -0,0 +1,19 @@
+package GitHub::API;
+
+# ABSTRACT: The great new GitHub::API!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
