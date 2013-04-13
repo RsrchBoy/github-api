65c68f9c51e30af09f8f651a02484d8b0245abd2
Chris Weyl <cweyl@alumni.drew.edu>
2013-04-12 17:04:55 -0700

    forks() now returns an array ref of Repo instances

--- lib/GitHub/API/Repo.pm
+++ lib/GitHub/API/Repo.pm
@@ -28,14 +28,73 @@ sub info { $_[0]->{repo_info} //= $_[0]->_get_single }
 sub is_fork   { !! $_[0]->info->{fork}     }
 sub has_forks { $_[0]->info->{forks_count} }
 
+# full_name: userid/repo
+# name: repo
+# owner->login: userid
+
+#sub raw_forks {
+    #my $self = shift @_;
+
+    #return $self->{raw_forks}
+        #if $self->{raw_forks};
+
+    #local $self->{url} = "$self->{url}/forks";
+    #return $self->{raw_forks} = $self->_get();
+#}
+
 sub forks {
     my $self = shift @_;
 
-    return $self->{forks} if $self->{forks};
+    return $self->{forks}
+        if $self->{forks};
+    return $self->{forks} = []
+        unless $self->has_forks;
 
-    local $self->{url} = "$self->{url}/forks";
-    return $self->{forks} = $self->_get()
+
+    my $forks = do {
+        local $self->{url} = "$self->{url}/forks";
+        $self->_get;
+    };
+
+    ### and build each into their own repo instance...
+    my $class = ref $self;
+    my @my_forks = map {
+        bless {
+            %$self,
+            all_forks => undef,
+            raw_forks => undef,
+            repo_info => $_,
+            url       => "user/$_->{full_name}",
+        }, $class
+    } @$forks;
+
+    $self->{raw_forks}    = $forks;
+    return $self->{forks} = \@my_forks; #$self->_get();
 }
+
+sub all_child_forks {
+    my $self = shift @_;
+
+    return $self->{child_forks}
+        if $self->{child_forks};
+
+    my @forks;
+    push @forks, @{ $_->all_child_forks }
+        for $self->forks;
+
+    return $self->{child_forks} = \@forks;
+}
+
+#sub all_forks {
+    #my $self = shift @_;
+
+    #return $self->{all_forks} if $self->{all_forks};
+
+    #local $self->{url} = "$self->{url}/forks";
+    #my $raw_forks6 = $self->_get();
+
+    #return $self->{forks} = $self->_get();
+#}
 
 !!42;
 __END__
